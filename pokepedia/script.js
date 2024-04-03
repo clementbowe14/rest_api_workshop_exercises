19af6f6fd17a01aa9bf64c7a88f7d1424894bd58
Clement Bowe <cbowe023@fiu.edu>
2024-04-03 02:16:51 -0400

    add finished version

--- pokepedia/script.js
+++ pokepedia/script.js
@@ -1,13 +1,31 @@
 
+const form = document.getElementById("pokemon_search_form")
 
+/**
+ when the submit button is clicked this function creates a GET request to https://pokeapi.co/api/v2/pokemon/{name}
+ if the search is successful it will create a card showing the information of the pokemon.
+ **/
 
-async function getPokemon(name) {
-    console.log(`"https://pokeapi.co/api/v2/pokemon/${name}`)
+form.addEventListener('submit',  async (event) => {
+    event.preventDefault();
+    let name = document.getElementById("pokemon_name").value
+    console.log(name)
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-    const pokemon = await response.json();
-    return pokemon
-}
+    console.log(response)
+    data = await response.json()
+    console.log(data)
+    let pokemonName = data.name
+    let pokemonType = data.types
+    let pokemonWeight = data.weight
+    
+    let card = document.createElement('div');
+    card.innerHTML = `
+    <div> Pokemon Name: ${pokemonName}</div>
+    <div> Pokemon Type: ${pokemonType[0].type.name}</div>
+    <div> Pokemon Weight:${pokemonWeight}</div>
+    `
 
-const data = getPokemon("charizard")
-
-console.log(data);
+    // clear result of previous search and fill 
+    document.getElementById('search_results').innerHTML = ''
+    document.getElementById('search_results').appendChild(card);
+})
